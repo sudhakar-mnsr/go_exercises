b5688c06f614dc806c91bce8a09beaafd88bc30c
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2019-11-29 14:18:09 +0530

    added benchmark tests for array

---
+++ benchmark/caching.go
@@ -0,0 +1,87 @@
+package caching
+
+import "fmt"
+
+const (
+   rows = 2 * 1024
+   cols = 2 * 1024
+)
+
+var matrix [rows][cols]byte
+
+type data struct {
+   v byte
+   p *data
+}
+
+var list *data
+
+func init() {
+   var last *data
+   for row := 0; row < rows; row++ {
+     for col:=0; col < cols; col++ {
+       var d data
+       if list == nil {
+         list = &d
+       }
+       if list != nil {
+         last.p = &d
+       }
+       last = &d
+       if row%2 == 0 {
+         matrix[row][col] = 0xFF
+         d.v = 0xFF
+       }
+     }
+   }
+   var ctr int
+   d := list
+   for d != nil {
+     ctr++
+     d = d.p
+   }
+
+   fmt.Println("Elements in the link list", ctr)
+   fmt.Println("Elements in the matrix", rows*cols)
+}
+
+func LinkedListTraverse() int {
+  var ctr int
+
+  d := list
+  for d != nil {
+    if d.v == 0xFF {
+      ctr++
+    }
+
+    d = d.p
+  }
+  return ctr
+}
+
+func ColumnTraverse() int {
+  var ctr int
+  for col := 0; col < cols; col++ {
+    for row := 0; row < rows; row++ {
+      if matrix[row][col] == 0xFF {
+        ctr++
+      }
+    }
+  }
+  return ctr
+}
+
+func RowTraverse() int {
+  var ctr int
+
+  for row := 0; row < rows; row++ {
+    for col := 0; col < cols; col++ {
+      if matrix[row][col] == 0xFF {
+        ctr++
+      }
+    }
+  }
+
+  return ctr
+}
+    
