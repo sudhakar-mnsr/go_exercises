8187984fc320b835853ab8e53496bf45908fb7e0
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2019-12-02 15:08:38 +0530

    added slice semantics for loop

---
+++ slice4.go
@@ -0,0 +1,21 @@
+// Sample program to show how the for range has both value and pointer semantics
+
+package main
+
+import "fmt"
+
+func main() {
+   // Using the value semantic form of the for range
+   
+   friends := []string{"Annie", "Betty", "Charley", "Doug", "Edward"}
+   for _, v := range friends {
+      friends = friends[:2]
+      fmt.Printf("v[%s]\n", v)
+   }
+   // Using the pointer semantic for of the for range
+   friends = []string{"Annie", "Betty", "Charley", "Doug", "Edward"}
+   for i := range friends {
+      friends = friends[:2]
+      fmt.Printf("v[%s]\n", friends[i])
+   }
+}
