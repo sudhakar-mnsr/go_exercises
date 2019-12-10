e60af10e9deaa4178bbf9a930c8d13d35a2b55eb
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2019-12-10 12:43:27 +0530

    multiple errors from single function

---
+++ err_handle2.go
@@ -0,0 +1,40 @@
+// Sample program to show how to use error variables to help the 
+// caller determine the exact error being returned.
+package main
+
+import (
+   "errors"
+   "fmt"
+)
+
+var (
+   // ErrBadRequest is returned when there are problems with the request.
+   ErrBadRequest = errors.New("Bad Request")
+   // ErrPageMoved is returned when a 301/302 is returned.
+   ErrPageMoved = errors.New("Page Moved")
+)
+
+func main() {
+   if err := webCall(true); err != nil {
+      switch err {
+      case ErrBadRequest:
+              fmt.Println("Bad Request Occured")
+              return
+      case ErrPageMoved:
+              fmt.Println("The page moved")
+              return
+      default:
+              fmt.Println(err)
+              return
+      }
+   }
+   fmt.Println("Life is good")
+}
+
+// webCall performs a web operation.
+func webCall(b bool) error {
+   if b {
+      return ErrBadRequest
+   }
+   return ErrPageMoved
+} 
