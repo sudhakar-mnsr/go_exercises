ac4b337a7b32e8af7072dc35ffb07f917e15ebf9
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2019-12-11 11:51:21 +0530

    added more code

--- example4.go
+++ example4.go
@@ -50,4 +50,32 @@ func (c *client) TypeAsContext() {
    }
 }
     
+// temporary is declared to test for the existence of the method coming
+// from the net package
+type temporary interface {
+   Temporary() bool
+}
 
+// BehaviorAsContext shows how to check for the behavior of an interface
+// that can be returned from the net package.
+func (c *client) BehaviorAsContext() {
+   for {
+      line, err := c.reader.ReadString('\n')
+      if err != nil {
+         switch e := err.(type) {
+            case temporary:
+               if !e.Temporary() {
+                  log.Println("Temporary: Client leaving chat")
+                  return
+               }
+            default:
+               if err == io.EOF {
+                  log.Println("EOF: Client leaving chat")
+                  return
+               }
+               log.Println("read-routine", err)
+            }
+      }
+      fmt.Println(line)
+   }
+} 
